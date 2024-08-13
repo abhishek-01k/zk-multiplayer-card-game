7ccdd0151cdbcaa984a28281744690e18160d97c
kamalbuilds <geniusamansingh@gmail.com>
2024-08-13 17:26:00 +0530

    fix the contract

--- smartcontract/contracts/ZkShuffleGame.sol
+++ smartcontract/contracts/ZkShuffleGame.sol
@@ -4,31 +4,44 @@ pragma solidity ^0.8.17;
 import "@zk-shuffle/contracts/contracts/shuffle/IShuffleStateManager.sol";
 import "@zk-shuffle/contracts/contracts/shuffle/IBaseGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-contract ZkShuffleGame is Ownable {
+contract ZkShuffleGame is IBaseGame, Ownable {
+    using EnumerableSet for EnumerableSet.AddressSet;
 
     enum GameState { WaitingForPlayers, Shuffling, Dealing, InProgress, Finished }
+    enum DeckConfig { Deck5Card, Deck30Card, Deck52Card }
 
     struct Player {
         address playerAddress;
-        bool hasJoined;
         uint256 hand;
+        bool isAI;
+        bool hasJoined;
     }
 
     GameState public gameState;
     Player[] public players;
     uint256 public maxPlayers;
     uint256 public pot;
+    DeckConfig public deckConfig;
+    EnumerableSet.AddressSet private activePlayers;
+
+    IShuffleStateManager public zkShuffle;
+    uint256 public gameId;
 
     event PlayerJoined(address indexed player);
     event GameStarted();
     event PlayerHandDealt(address indexed player, uint256 hand);
     event GameEnded(address indexed winner, uint256 pot);
 
-        IShuffleStateManager public zkShuffle;
-    constructor(uint256 _maxPlayers, address owner) Ownable(owner) {
+    constructor(uint256 _maxPlayers, DeckConfig _deckConfig, address _zkShuffleAddress) Ownable(msg.sender) {
+        require(_maxPlayers > 0, "Max players must be greater than zero");
+        require(_zkShuffleAddress != address(0), "Invalid Shuffle Manager address");
+
         maxPlayers = _maxPlayers;
         gameState = GameState.WaitingForPlayers;
+        deckConfig = _deckConfig;
+        zkShuffle = IShuffleStateManager(_zkShuffleAddress);
     }
 
     function joinGame() external payable {
@@ -38,12 +51,30 @@ contract ZkShuffleGame is Ownable {
 
         players.push(Player({
             playerAddress: msg.sender,
-            hasJoined: true,
-            hand: 0
+            hand: 0,
+            isAI: false,
+            hasJoined: true
         }));
 
+        activePlayers.add(msg.sender);
         pot += msg.value;
         emit PlayerJoined(msg.sender);
+
+        if (players.length == maxPlayers) {
+            startGame();
+        }
+    }
+
+    function addAIPlayer() external onlyOwner {
+        require(gameState == GameState.WaitingForPlayers, "Game is not accepting players");
+        require(players.length < maxPlayers, "Game is full");
+
+        players.push(Player({
+            playerAddress: address(0),
+            hand: 0,
+            isAI: true,
+            hasJoined: true
+        }));
 
         if (players.length == maxPlayers) {
             startGame();
@@ -53,14 +84,20 @@ contract ZkShuffleGame is Ownable {
     function startGame() internal {
         gameState = GameState.Shuffling;
         emit GameStarted();
-        zkShuffle.shuffle();
+
+        gameId = zkShuffle.createShuffleGame(uint8(players.length));
+
+        bytes memory next = abi.encodeWithSelector(this.dealHands.selector);
+        zkShuffle.shuffle(gameId, next);
     }
 
     function dealHands() external onlyOwner {
         require(gameState == GameState.Shuffling, "Game is not in shuffling state");
 
         for (uint256 i = 0; i < players.length; i++) {
-            players[i].hand = zkShuffle.dealCard();
+            uint256 playerId = zkShuffle.getPlayerIdx(gameId, players[i].playerAddress);
+            BitMaps.BitMap256 memory dealtCards = zkShuffle.getDecryptRecord(gameId, playerId);
+            players[i].hand = uint256(dealtCards._data); // This is a simplification; actual implementation depends on the card data.
             emit PlayerHandDealt(players[i].playerAddress, players[i].hand);
         }
 
@@ -69,22 +106,40 @@ contract ZkShuffleGame is Ownable {
 
     function endGame(uint256 winnerIndex) external onlyOwner {
         require(gameState == GameState.InProgress, "Game is not in progress");
-        
+        require(winnerIndex < players.length, "Invalid winner index");
+
         Player memory winner = players[winnerIndex];
         require(winner.hasJoined, "Winner must be a player");
 
-        payable(winner.playerAddress).transfer(pot);
+        if (winner.isAI) {
+            activePlayers.remove(address(0));
+        } else {
+            activePlayers.remove(winner.playerAddress);
+            payable(winner.playerAddress).transfer(pot);
+        }
+
         emit GameEnded(winner.playerAddress, pot);
 
         gameState = GameState.Finished;
     }
 
     function isPlayerInGame(address player) internal view returns (bool) {
-        for (uint256 i = 0; i < players.length; i++) {
-            if (players[i].playerAddress == player) {
-                return true;
-            }
-        }
-        return false;
+        return activePlayers.contains(player);
+    }
+
+    function cardConfig() external override view returns (DeckConfig) {
+        return deckConfig;
+    }
+
+    function allowJoinGame() external onlyOwner {
+        require(gameState == GameState.WaitingForPlayers, "Game not waiting for players");
+        bytes memory next = abi.encodeWithSelector(this.moveToShuffleStage.selector);
+        zkShuffle.register(gameId, next);
+    }
+
+    function moveToShuffleStage() external onlyOwner {
+        require(gameState == GameState.WaitingForPlayers, "Game not in waiting state");
+        bytes memory next = abi.encodeWithSelector(this.dealHands.selector);
+        zkShuffle.shuffle(gameId, next);
     }
 }
